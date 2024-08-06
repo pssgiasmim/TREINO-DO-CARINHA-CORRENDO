30bc88f918674e3028b0c8be21264332b0d820b2
pssgiasmim <iasmimpassing@gmail.com>
2024-08-06 14:00:46 -0300

    Script PLAYER
    1. Método que faz o carinha se mover e impede que o player saia dos limites da tela.
         1.1. Linha que faz o personagem se mover para os lados.
         1.2. Trecho que impede que o player (o carinha) saia dos limites da tela.
    
    2. Método que "ativa" a linha que faz o personagem se mover para os lados.

---
+++ TREINO DO CARINHA CORRENDO - UNITY/Assets/Script/Player.cs
@@ -0,0 +1,43 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] Rigidbody2D rigidbody2D;
+    float direction, speed = 10
+
+    //Método que faz o carinha se mover e impede que o player saia dos limites da tela.
+    public void Move()
+    {
+        //Linha que faz o personagem se mover para os lados.
+        rigidbody2D.velocity = new Vector2(direction * speed, 0);
+
+        //Trecho que impede que o player (o carinha) saia dos limites da tela.
+        if (transform.position.x < GameManager.instance.ScreenBounds.x * -1)
+        {
+            transform.position = new Vector2(GameManager.instance.ScreenBounds.x * -1, transform.position.y);
+        }
+        else
+        {
+            if (transform.position.x > GameManager.instance.ScreenBounds.x)
+            {
+                transform.position = new Vector2(GameManager.instance.ScreenBounds.x, transform.position.y);
+            }
+        }
+
+    }
+
+    //Trecho que "ativa" a linha que faz o personagem se mover para os lados.
+    public void Update()
+    {
+        direction = Input.GetAxis("Horizontal");
+
+        Move();
+    }
+
+}
